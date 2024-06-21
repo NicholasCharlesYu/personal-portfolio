57d6c0f1464c0f7895038f18b75612359635178a
Nicholas Yu <ncyu@usc.edu>
2024-06-21 15:00:13 -0700

    Fixed NavBar.js, forgot 'c' in 'className' for skills and projects, causing them to not show up. Also added import to bootstrap css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -40,8 +40,8 @@ export const NavBar = () =>{
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivelink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" lassName={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivelink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" lassName={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivelink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivelink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActivelink('projects')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
