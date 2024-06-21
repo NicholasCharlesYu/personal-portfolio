fdd60aef2c304394f46b3fbcea08739237e0ba75
Nicholas Yu <ncyu@usc.edu>
2024-06-21 14:46:09 -0700

    Added NavBar component to App.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
