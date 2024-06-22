17e403f752afeb56c6dfee06865836d878968dcf
Nicholas Yu <ncyu@usc.edu>
2024-06-22 13:20:22 -0700

    saving, added comments and finished functions to handle typed/deleted text on header

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,10 +5,46 @@ import headerImg from "../assets/img/header-img.svg";
 import React from "react";
 
 export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Web Developer", "Web Designer", "UI/UX Des"];
-    const [text, setText] = useState('');
+    //State variables to manage text display looping animation
+    const [loopNum, setLoopNum] = useState(0); // Tracks the current loop number
+    const [isDeleting, setIsDeleting] = useState(false); //Indicates if the text is being deleted or "typed out"
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Des"]; // Array of strings to rotate through
+    const [text, setText] = useState(''); //Current Text on display
+    const [delta, setDelta] = useState(300 - Math.random() * 100) // Time interval for typing effect
+    const period = 2000; // Period of time to wait before deleting text
+
+    //starts interval for typing effect
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick(); // call tick funct at every interval
+        }, delta)
+        
+        // Cleanup Function to clear interval when component unmounts
+        return () => {clearInterval(ticker)};
+    }, [text]); //Dependency array to run effect only when 'text' changes
+
+    //Funct to handle typing and deleting effect
+    const tick = () => {
+        let i = loopNum % toRotate.length; //Calc current index
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }
+        else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
 
     return (
        <section className="banner" id="home">
